c313fedb15313b7c1db5091c03a18d5360dfbf8f
Brett A Jones <evil_greven@yahoo.com>
2014-12-02 16:40:54 -0600

    Create system.php

---
+++ system.php
@@ -0,0 +1,10 @@
+<?	//Purpose:		This defines paths for the database
+	//Original Author:	Brett Jones
+	//Creation Date:	9/14/2013
+	//Modification Date:	11/18/2013
+	//Modification Purpose:	Audit for Coding Convention exchange
+$TEXT['database_user'] = "root";
+$TEXT['database_password'] = "it_wasnt_this_password_sorry";
+$TEXT['database_address'] = "localhost";
+$TEXT['database_name'] = "ldt";
+?>
